8b9a211559fbe6773e2be057a18178a35fc80915
Jacob Shaw <jacob.shaw@unb.ca>
2018-09-07 15:58:13 -0300

    Still Developing

--- process.c
+++ process.c
@@ -1,21 +1,30 @@
 //process.c
 
 #include <stdlib.h>
+#include <string.h>
 #include "process.h"
-Proc *P;
-int PROC_init(char j, int d, int a){
-  P = malloc(sizeof(Proc));
-  if(P == null) return 0; //failed
+
+
+struct Proc PROC_init(char j, int d, int a,char *u){
+  struct Proc *P;
+  P = malloc(sizeof(struct Proc));
+  if(P == NULL) return *P; //failed
   P->job = j;
   P->dur = d;
   P->arrive =a;
-  return 1;
+  P->user = strdup(u);
+  return *P;
 }
 
-PROC_destroy(){
+void PROC_destroy(struct Proc *P){
   free(P);
 }
 
-int PROC_arrived(int time){
+int PROC_arrived(struct Proc *P,int time){
   return (P->arrive <= time);
+}
+int PROC_work(struct Proc *P){
+	if(P->dur == 0){return 0;}
+	P->dur= P->dur-1;
+	return 1;
 }
